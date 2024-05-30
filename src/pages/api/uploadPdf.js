2cb92753da82566fd179cc78f9ffefd599bcb75c
Jay Willey <jasoncwilley@users.noreply.github.com>
2024-05-30 05:24:09 -0400

    Update uploadPdf.js
    updated form submission to server

--- src/pages/api/uploadPdf.js
+++ src/pages/api/uploadPdf.js
@@ -1,12 +1,15 @@
+
+require('dotenv').config();
 import mysql from 'mysql2';
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const connection = mysql.createConnection({
-      host: '89.116.51.37',
-      database: 'planitcontract_wordpress',
-      user: 'remote',
-      password: 'remote'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port: process.env.DB_PORT
     });
 
     console.log('Connected to database');
@@ -14,7 +17,7 @@ export default function handler(req, res) {
     const { userId, txId } = req.body;
     const formattedTxId = `https://arweave.net/${txId}`;
 
-    const sql = `INSERT INTO uploads (upload_id, user_id, image_link, metadata_link) VALUES (NULL, ?, ?, ?)`;
+    const sql = `INSERT INTO planitcontract_wordpress.uploads (upload_id, user_id, image_link) VALUES (NULL, ?, ?)`;
     const values = [userId, formattedTxId];
 
     connection.query(sql, values, (err, result) => {
